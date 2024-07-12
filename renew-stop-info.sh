4939e0301671af6fb23d5ff5cb816c3de1b6c287
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2024-07-12 16:40:50 +0800

    新增 renew-stop-info.sh， 放入 cron， 每晚更新站牌資訊

---
+++ renew-stop-info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /opt/tdx7984
+source tdx-credential.sh
+
+for ct in $(grep -Po '\b[A-Z]\w+$' cities.csv) ; do
+# for ct in TaitungCounty ; do # 省時測試版，只更新台東縣
+    curl -H 'accept: application/json' -H "authorization: Bearer $TDX_ACCESS_TOKEN" "https://tdx.transportdata.tw/api/basic/v2/Bus/StopOfRoute/City/$ct" > city-stops/$ct.json
+    sleep 8
+done
+
+python3 sqlify.py -f -d ./city-stops/ routes_stops.sqlite3
+
